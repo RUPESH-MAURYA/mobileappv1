4a587f0c6c85d4807e79b95a5b44d4ef8520cbdc
LAPTOP-T5929UCB\rupes <rupeshmaurya.998@gmail.com>
2020-08-16 18:17:32 +0530

    MobileApp

---
+++ main.py
@@ -0,0 +1,81 @@
+from kivy.app import App
+from kivy.lang import Builder 
+from kivy.uix.screenmanager import ScreenManager ,Screen 
+import json,glob, random
+from pathlib import Path
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from datetime import datetime
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current="sign_up_screen"
+    pass
+
+    def login(self,username,password):
+        with open("users.json") as file:
+            users=json.load(file)
+        if username in users and users[username]['password']==password:
+            self.manager.current="login_screen_success"
+        else:
+            self.ids.wrong_login.text="Wrong username or Password"
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
+
+    def back(self):
+        self.manager.current="login_screen"
+
+    def add_user(self,username,password):
+        
+        with open("users.json") as file: 
+            users = json.load(file)
+
+
+        users[username]={'username':username,'password':password,'created':datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}
+        
+        with open("users.json" ,'w') as file:
+            json.dump(users,file)
+
+        self.manager.current="sign_up_sucess"
+
+class SignUpSucess(Screen):
+    def loginpage(self):
+        self.manager.transition.direction='right'
+        self.manager.current="login_screen"
+
+class LoginScreenSuccess(Screen):
+    
+    def log_out(self):
+        self.manager.current="login_screen"
+
+    def get_quotes(self,feeling):
+        feel = feeling.strip().lower()       
+        avilable_feeling = glob.glob("quotes\*txt")
+        avilable_feeling = [Path(filename).stem for filename in avilable_feeling]
+        if feel in avilable_feeling:
+            with open(f"quotes/{feel}.txt",encoding="utf8") as file:
+                quotes = file.readlines()
+            self.ids.quotes.text = random.choice(quotes)        
+        else:
+           self.ids.quotes.text ="Try other input"
+
+
+class ImageButton(ButtonBehavior, HoverBehavior,Image):
+    pass
+
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__=="__main__":
+    MainApp().run()
